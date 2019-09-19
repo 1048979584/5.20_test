f0ba6c592bd2012e673ea8612a5cfce71fec8520
1048979584 <50434742+1048979584@users.noreply.github.com>
2019-09-19 11:28:17 +0800

    Update to voice.py

--- to voice.py
+++ to voice.py
@@ -43,3 +43,24 @@ with open(title+'.txt','w+',encoding='utf-8')as f:
     content1 = ''.join(content).replace('\xa0', '')
     content2 = ''.join(content1).replace('\n', '')
     f.writelines(content2)
+if __name__=="__main__":
+    print('开始爬取，请稍候...')
+    header={
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+        'Cookie':'Hm_lvt_3806e321b1f2fd3d61de33e5c1302fa5=1568816590; bcolor=; font=; size=; fontcolor=; width=; Hm_lpvt_3806e321b1f2fd3d61de33e5c1302fa5=1568816903'
+    }
+    urls=[
+        'http://www.shuquge.com/txt/63542/9645081.html',
+        'http://www.shuquge.com/txt/63542/9645082.html',
+        'http://www.shuquge.com/txt/63542/9645082.html',
+        'http://www.shuquge.com/txt/63542/17214480.html',
+        'http://www.shuquge.com/txt/63542/17219206.html',
+        'http://www.shuquge.com/txt/63542/17220023.html',
+        'http://www.shuquge.com/txt/63542/17220374.html',
+        'http://www.shuquge.com/txt/63542/17223363.html',
+    ] # 将url链接放到列表里面，每次取出一个进行爬取
+    for url in urls:
+        getText(url,header)
+    print('恭喜你，爬取完成。')    
+    
+    
